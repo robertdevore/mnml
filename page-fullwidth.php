0a12fb8f2ba08a99ab773d5272970cd432da868d
Devio Digital <deviodigital@gmail.com>
2015-09-08 12:05:33 -0400

    added full width page template

---
+++ page-fullwidth.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * The template for displaying full width pages (no sidebar)
+ *
+ * @package MNML
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="col-lg-12 content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
